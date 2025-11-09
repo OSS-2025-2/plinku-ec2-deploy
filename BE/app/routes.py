c6fe9d22ef6fd77b8e77ebdcb3a7797b60489a2e
KimSM <20211467@edu.hanbat.ac.kr>
2025-11-09 20:28:40 +0900

    Feat: 주차장, 충전소  탐색 기 본값 수정 , 주차장조회에서 주차장,충전소 조회로 기능 전환
    - ev_charger 기본값 설정: 버튼 클릭 시 주차장/충전소 리스트 기본 출력
    - 주차장,충전소 정보 조회 중 주차장과 충전소 구분하는 변수 추가
    Resolves: #2,#3

--- BE/app/routes.py
+++ BE/app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify,request
-from .sample_data import PARKINGS, RESERVATIONS
+from .sample_data import PARKINGS
 main = Blueprint('main', __name__)
 
 
@@ -15,14 +15,32 @@ def list_parkings():
     ev_filter = request.args.get("ev_charger")              # true/false
     keyword = request.args.get("keyword", default="").lower()
 
+    # 버튼 클릭에 따라 기본값 전달 가능
+    # 기본: 주차장 리스트
+    ev_filter = request.args.get("ev_charger")
+    if ev_filter is None:
+        ev_filter = "false"
+
+
     #주차장 탐색 필터링
     """수업 중 배운 지능형 리스트를 사용하여 results를 생성"""
     results = [
         p for p in PARKINGS.values()
         if (not congestion_filter or p.get("congestion") == congestion_filter)
-        and (not ev_filter or str(p.get("ev_charger", False)).lower() == ev_filter.lower())
+        and (str(p.get("ev_charger", False)).lower() == ev_filter.lower())
         and (keyword in p.get("parking_name", "").lower() or keyword in p.get("address", "").lower())
     ]
+    #원래문단
+    #results = []
+    #for p in PARKINGS.values():
+    # if congestion_filter and p.get("congestion") != congestion_filter:
+        #continue
+    # if ev_filter and str(p.get("ev_charger", False)).lower() != ev_filter.lower():
+        #continue
+    # if not (keyword in p.get("parking_name", "").lower() or
+            # keyword in p.get("address", "").lower()):
+        #continue
+    #results.append(p)
 
     #정렬
     reverse = (order == "desc")
@@ -60,7 +78,8 @@ def list_parkings():
     }), 200
 
 
-#주차장 정보 조회 
+
+
 @main.route('/api/parkings/<int:id>', methods=['GET'])
 def get_parking(id):
     parking = PARKINGS.get(id)
@@ -68,12 +87,12 @@ def get_parking(id):
     if not parking:
         return jsonify({
             "status": "fail",
-            "message": "주차장 정보를 불러올 수 없습니다.",
+            "message": "주차장 또는 충전소 정보를 불러올 수 없습니다.",
             "error_code": "NOT_FOUND"
         }), 404
 
     return jsonify({
         "status": "success",
-        "message": "주차장 정보를 불러왔습니다.",
+        "message": f"{'충전소' if parking['type'] == 'charger' else '주차장'} 정보를 불러왔습니다.",
         "data": parking
-    }),200
+    }), 200
