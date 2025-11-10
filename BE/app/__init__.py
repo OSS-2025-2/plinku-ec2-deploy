49d77a08702a05d145be4d2f2fd1a6d486869469
KimSM <20211467@edu.hanbat.ac.kr>
2025-11-10 22:15:44 +0900

    Initialize project structure

--- BE/app/__init__.py
+++ BE/app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
-from app.routes import main
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(main)
+
+    # 나중에 기능이 생기면 여기에 등록
+    # 예시:
+    # from app.routes.parking_routes import parking_bp
+    # app.register_blueprint(parking_bp)
+
+    @app.route('/')
+    def home():
+        return "서버가 정상적으로 실행 중입니다!"
+
     return app
