c9699a0254ffec8beb11d984106e5c2c6e6df6e8
정용성 <JYS-9542@naver.com>
2025-11-23 19:32:16 +0900

    user테이블 추가

--- BE/app/models/users.py
+++ BE/app/models/users.py
@@ -1,6 +1,12 @@
-from BE.app.config import db
+from app.config import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
